d267150e8786c98219872e6a302aee05e6b1690b
이백산 <ibaeksan@ibaegsan-ui-MacBookAir.local>
2026-07-05 22:34:06 +0900

    fix: add missing numpy import

--- technical_signal_dashboard.py
+++ technical_signal_dashboard.py
@@ -12,6 +12,7 @@
 
 import streamlit as st
 import pandas as pd
+import numpy as np
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from datetime import datetime, timedelta
